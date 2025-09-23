7a3e5a9c23796d8d762e8a359fb7499e209f041f
WorldDrknss <cdshowalter2006@yahoo.com>
2025-09-23 09:43:35 -0700

    refactor(terraform): update formatting for consistency in main.tf and .gitlab-ci.yml

--- scripts/terraform/main.tf
+++ scripts/terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {}
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 6.0"
     }
   }
@@ -18,26 +18,26 @@ data "aws_availability_zones" "available" {}
 # VPC + Subnets
 # -------------------------
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "~> 6.2"
 
   name = "${var.cluster_name}-vpc"
   cidr = "10.123.0.0/16"
 
-  azs = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
   private_subnets = ["10.123.0.0/24", "10.123.1.0/24", "10.123.2.0/24"]
-  public_subnets = ["10.123.64.0/18", "10.123.128.0/18", "10.123.192.0/18"]
+  public_subnets  = ["10.123.64.0/18", "10.123.128.0/18", "10.123.192.0/18"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
 
   public_subnet_tags = {
-    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/role/elb"                    = "1"
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
 
   private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/role/internal-elb"           = "1"
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
 }
@@ -46,10 +46,10 @@ module "vpc" {
 # EKS Cluster
 # -------------------------
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  name = var.cluster_name
+  name               = var.cluster_name
   kubernetes_version = "1.33"
 
   addons = {
@@ -63,20 +63,20 @@ module "eks" {
     }
   }
 
-  vpc_id = module.vpc.vpc_id
+  vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  enable_irsa = true
-  authentication_mode = "API_AND_CONFIG_MAP"
+  enable_irsa                              = true
+  authentication_mode                      = "API_AND_CONFIG_MAP"
   enable_cluster_creator_admin_permissions = true
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 3
-      max_size = 3
-      min_size = 1
+      desired_size   = 3
+      max_size       = 3
+      min_size       = 1
       instance_types = ["t3.xlarge"]
-      capacity_type = "ON_DEMAND"
+      capacity_type  = "ON_DEMAND"
     }
   }
 }
