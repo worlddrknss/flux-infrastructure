b54a4c9ba7837beb2609e77cca103dba9a6979a5
WorldDrknss <cdshowalter2006@yahoo.com>
2025-09-23 09:49:55 -0700

    refactor(outputs): remove kubeconfig output definition

--- scripts/terraform/outputs.tf
+++ scripts/terraform/outputs.tf
@@ -12,9 +12,3 @@ output "oidc_provider" {
   description = "OIDC provider for IRSA"
   value       = module.eks.oidc_provider
 }
-
-output "kubeconfig" {
-  description = "Raw kubeconfig to access the EKS cluster"
-  value       = module.eks.kubeconfig
-  sensitive   = true
-}
